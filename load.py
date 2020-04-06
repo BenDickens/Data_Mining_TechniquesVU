98c0399ed4b0b2c403d8961f398523530518220d
BenDickens <bendickens@zoho.com>
2020-04-06 14:04:45 +0200

    Cleaner with variables as columns

--- load.py
+++ load.py
@@ -6,10 +6,20 @@ import numpy as np
 def read():
     data = pd.read_csv(open('./dataset_mood_smartphone.csv'))
     patients = np.unique(list(data.id))
+    counter = 0
     for i in patients:
-    	patient_data = data.loc[data.id==i]
-    	patient_data.to_csv("./patientData/patient" + i + ".csv")
+        patDat = data.loc[data.id==i]
+        del patDat['id']
+        del patDat['Unnamed: 0']
+        patDat['time'] = pd.to_datetime(patDat['time']).dt.date
+        #patDat.index = pd.to_datetime(patDat['time'])
+        #del patDat['time']
+        patDat['id'] = range(len(patDat))
+        print(patDat.columns)
+        cleanPat = patDat.pivot_table(index="time", columns="variable", values="value", aggfunc=np.mean)
+        cleanPat.to_csv("./patientData/patient" + i + ".csv")
+
         
 
 if __name__ == '__main__':
-	read()
+    read()
