fc7ea21f92be42040b4270425f5ba2a7fd4dfcab
vivdenx <46710396+vivdenx@users.noreply.github.com>
2020-04-14 14:52:07 +0200

    Complete benchmark

--- benchmark.py
+++ benchmark.py
@@ -4,20 +4,20 @@ from sklearn.metrics import accuracy_score, mean_squared_error
 import math
 
 
-def create_benchmark(folderpath, benchmark_filepath):
+def create_benchmark(patient_folder, benchmark_filename):
     """
     Loops over the data in the folder path. Adds a column with patient number, based on the filename.
         Checks whether the dates are consecutive, and only adds the row to the file if it is.
         Creates benchmark from the previous day. Removes all unnecessary columns before writing
-        to a new file in benchmark_filepath.
+        to a new file in benchmark_filename.
 
-    :param folderpath: path to the folder that contains all patient data.
-    :param benchmark_filepath: path to the file to which the output should be written.
+    :param patient_folder: path to the folder that contains all patient data.
+    :param benchmark_filename: path to the file to which the output should be written.
     """
     complete_data = pd.DataFrame()
 
-    for filename in os.listdir(folderpath):
-        data = pd.read_csv(folderpath + filename)
+    for filename in os.listdir(patient_folder):
+        data = pd.read_csv(patient_folder + filename)
 
         # Create patient number column
         patientno = filename.replace("patientAS14.", "").replace(".csv", "")
@@ -41,16 +41,16 @@ def create_benchmark(folderpath, benchmark_filepath):
         data = data[cols]
 
         complete_data = complete_data.append(data)
-    complete_data.to_csv(benchmark_filepath)
+    complete_data.to_csv(benchmark_filename)
 
 
-def evaluate_benchmark(benchmark_filepath):
+def evaluate_benchmark(benchmark_filename):
     """
     Evaluates the benchmark. Get accuracy and root mean square error (RSME) scores.
-    :param benchmark_filepath: path to the benchmark.
+    :param benchmark_filename: path to the benchmark.
     :return: accuracy, rmse
     """
-    data = pd.read_csv(benchmark_filepath)
+    data = pd.read_csv(benchmark_filename)
 
     y_true = data["mood"].astype(int).to_numpy()
     y_pred = data["benchmark"].astype(int).to_numpy()
@@ -65,10 +65,10 @@ def evaluate_benchmark(benchmark_filepath):
 
 
 def run():
-    folderpath = "./patientData/"
-    benchmark_filepath = "./benchmarkData/benchmark.csv"
-    create_benchmark(folderpath, benchmark_filepath)
-    evaluate_benchmark(benchmark_filepath)
+    patient_folder = "./patientData/"
+    benchmark_filename = "./benchmarkData/benchmark.csv"
+    create_benchmark(patient_folder, benchmark_filename)
+    evaluate_benchmark(benchmark_filename)
 
 
 if __name__ == "__main__":
