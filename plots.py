0e8249f4a62729d44fe9f21ea13bd9cb03f6cb49
vivdenx <46710396+vivdenx@users.noreply.github.com>
2020-04-15 14:45:38 +0200

    Plot for the average mood of the entire dataset.

---
+++ plots.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import csv
+import datetime
+
+def plot_population(population_csv):
+    x = []
+    y = []
+
+    with open(population_csv, 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        next(plots)
+        for row in plots:
+            x.append(row[0])
+            y.append(round(float(row[1]), 1))
+
+    x = [datetime.datetime.strptime(date, "%Y-%m-%d").date() for date in x]
+    ax = plt.gca()
+
+    formatter = mdates.DateFormatter("%m-%d")
+    ax.xaxis.set_major_formatter(formatter)
+
+    locator = mdates.AutoDateLocator()
+    ax.xaxis.set_major_locator(locator)
+
+    plt.plot(x, y)
+    plt.xlabel('Date')
+    plt.ylabel('Average mood score of the population')
+    plt.title('Average mood per day of the entire population')
+    plt.show()
+
+
+def run():
+    population_csv = "./patientDataARCleaned/population.csv"
+    plot_population(population_csv)
+
+
+if __name__ == "__main__":
+    run()
